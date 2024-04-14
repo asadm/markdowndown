2410a02b9e23547181554f6e326663ffece3e2b6
Asad Memon <asad.lionpk@gmail.com>
2024-04-14 10:35:02 -0700

    fix analytics.jsx

--- src/components/analytics.jsx
+++ src/components/analytics.jsx
@@ -4,12 +4,12 @@ mixpanel.init('1b44f6c7e06a13b45bb2231bb96e611f', {
   api_host: `https://ws.joinplayroom.com/__mix`
 });
 
-export function identify(uuid: any, data: any) {
+export function identify(uuid, data) {
   mixpanel.identify(uuid);
   mixpanel.people.set(data);
 }
 
-export function track(event: any, data: any) {
+export function track(event, data) {
   try {
     mixpanel.track(event, data);
   } catch (e) {}
